35f34c0cc66016146a453fb2e16fbe5af7f162e9
Bakr Mohamed <bakr.mohamed@linkdev.com>
2022-06-16 10:56:42 +0200

    Initial Commit

---
+++ MMOA/MMOAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMOAApp.swift
+//  MMOA
+//
+//  Created by Bakr mohamed on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MMOAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
